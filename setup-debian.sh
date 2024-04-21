e27c5f205c7f74bc63de17b213518600e53ee6ab
neoreuvenla <99553612+neoreuvenla@users.noreply.github.com>
2024-04-21 18:05:55 +0100

    script update

--- setup-debian.sh
+++ setup-debian.sh
@@ -6,7 +6,7 @@
 sudo sed -i 's/^\s*deb cdrom:/#deb cdrom:/' /etc/apt/sources.list
 
 # ENABLE DEBIAN BACKPORT REPO
-echo "deb http://deb.debian.org/debian bookworm-backports main" | sudo tee -a /etc/apt/sources.list.d/backports.list
+echo "deb http://deb.debian.org/debian bookworm-backports main contrib non-free" | sudo tee -a /etc/apt/sources.list
 sudo apt update
 # sudo apt -t bookworm-backports install <package-name> # for future use
 
@@ -46,17 +46,22 @@ sudo apt install -y git
 # REPLACE FIREFOX ESR WITH MOZILLA REPO FIREFOX
 sudo apt remove -y firefox-esr
 sudo install -d -m 0755 /etc/apt/keyrings
-wget -qO- https://packages.mozilla.org/apt/repo-signing-key.gpg | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc
-echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt/debian/ bookworm firefox" | sudo tee /etc/apt/sources.list.d/mozilla-firefox.list
-echo "Package: *
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+gpg -n -q --import --import-options import-show /etc/apt/keyrings/packages.mozilla.org.asc | awk '/pub/{getline; gsub(/^ +| +$/,""); if($0 == "35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3") print "\nThe key fingerprint matches ("$0").\n"; else print "\nVerification failed: the fingerprint ("$0") does not match the expected one.\n"}'
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+echo '
+Package: *
 Pin: origin packages.mozilla.org
-Pin-Priority: 1000" | sudo tee /etc/apt/preferences.d/mozilla-firefox
-sudo apt update && sudo apt install -y firefox
+Pin-Priority: 1000
+' | sudo tee /etc/apt/preferences.d/mozilla 
+sudo apt-get update && sudo apt-get install firefox
 
 # SIGNAL
-wget -qO- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor | sudo tee /usr/share/keyrings/signal-desktop-keyring.gpg >/dev/null
-echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' | sudo tee /etc/apt/sources.list.d/signal-xenial.list
-sudo apt update && sudo apt install -y signal-desktop
+wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
+cat signal-desktop-keyring.gpg | sudo tee /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
+  sudo tee /etc/apt/sources.list.d/signal-xenial.list
+sudo apt update && sudo apt install signal-desktop
 
 # LIBREOFFICE (uncomment to remove .deb and install flatpak version)
 # sudo apt remove --purge -y libreoffice*
@@ -74,17 +79,17 @@ sudo systemctl start tlp
 # POWERTOP
 sudo apt install -y powertop
 sudo powertop --calibrate
-echo -e '[Unit]\nDescription=PowerTOP auto-tune\n[Service]\nType=oneshot\nExecStart=/usr/sbin/powertop --auto-tune\n[Install]\nWantedBy=multi-user.target' | sudo tee /etc/systemd/system/powertop.service
+echo "powertop --auto-tune" | sudo tee -a /etc/rc.local > /dev/null
 sudo systemctl enable powertop.service
 sudo systemctl start powertop.service
 
 # UFW
-sudo apt install ufw -y
+sudo apt install -y ufw
 echo 'yes' | sudo ufw enable
 sudo ufw default deny incoming
 
 # MBPFAN (Macbook Pro Fans)
-sudo apt install mbpfan -y 
+sudo apt install -y mbpfan
 sudo systemctl enable mbpfan 
 sudo systemctl start mbpfan
 echo "min_fan_speed = 2000  # Minimum fan speed" | sudo tee -a /etc/mbpfan.conf
@@ -96,8 +101,8 @@ sudo apt install -y geary
 # PLYMOUTH (with darwin boot theme)
 sudo apt install -y plymouth
 git clone https://github.com/libredeb/darwin-plymouth
-sudo cp -R darwin/ /usr/share/plymouth/themes/
-sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/darwin/darwin.plymouth 100
+sudo cp -R darwin-plymouth/ /usr/share/plymouth/themes/
+sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/darwin-plymouth/darwin/darwin.plymouth 100
 sudo update-alternatives --set default.plymouth /usr/share/plymouth/themes/darwin/darwin.plymouth
 GRUB_CMDLINE="quiet splash"
 sudo sed -i "s/^GRUB_CMDLINE_LINUX_DEFAULT=\".*\"/GRUB_CMDLINE_LINUX_DEFAULT=\"$GRUB_CMDLINE\"/" /etc/default/grub
@@ -115,7 +120,7 @@ sudo apt install curl
 sudo curl -fsSLo /usr/share/keyrings/mullvad-keyring.asc https://repository.mullvad.net/deb/mullvad-keyring.asc
 echo "deb [signed-by=/usr/share/keyrings/mullvad-keyring.asc arch=$( dpkg --print-architecture )] https://repository.mullvad.net/deb/stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/mullvad.list
 sudo apt update
-sudo apt install -y mullvad-vpn
+sudo apt install mullvad-vpn
 
 
 # OPTIONAL BIGSUR THEMING (https://github.com/jothi-prasath/SmallSur)
@@ -124,7 +129,14 @@ read response
 response=${response,,}  # Converts to lowercase
 if [[ "$response" == "y" ]]; then
 echo "Installing theme..."
-    sudo apt install -y plank ulancher vala-panel-appmenu
+    sudo apt install -y plank vala-panel-appmenu
+    sudo apt update && sudo apt install -y gnupg
+    gpg --keyserver keyserver.ubuntu.com --recv 0xfaf1020699503176
+    gpg --export 0xfaf1020699503176 | sudo tee /usr/share/keyrings/ulauncher-archive-keyring.gpg > /dev/null
+    echo "deb [signed-by=/usr/share/keyrings/ulauncher-archive-keyring.gpg] \
+          http://ppa.launchpad.net/agornostal/ulauncher/ubuntu jammy main" \
+          | sudo tee /etc/apt/sources.list.d/ulauncher-jammy.list
+sudo apt update && sudo apt install ulauncher
     user_name="$USER"
     sudo apt install xfce4-appmenu-plugin appmenu-* -y
     sudo apt install xfce4-indicator-plugin xfce4-statusnotifier-plugin xfce4-power-manager xfce4-pulseaudio-plugin xfce4-notifyd -y
